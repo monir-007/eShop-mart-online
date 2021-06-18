f9438e5c96e23077406a016e8052e74b012d3c55
Monir Hossain <monir.hossain@northsouth.edu>
2021-06-19 03:35:23 +0600

    feat(user reset password): reset password mail setup

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,34 +1,51 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+@extends('user-view.layouts.app')
+@section('content')
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-        </div>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
+    <div class="breadcrumb">
+        <div class="container">
+            <div class="breadcrumb-inner">
+                <ul class="list-inline list-unstyled">
+                    <li><a href="{{route('home')}}">Home</a></li>
+                    <li class='active'>Forgot password</li>
+                </ul>
+            </div><!-- /.breadcrumb-inner -->
+        </div><!-- /.container -->
+    </div><!-- /.breadcrumb -->
 
-        <x-jet-validation-errors class="mb-4" />
+    <div class="body-content">
+        <div class="container">
+            <div class="sign-in-page">
+                <div class="row">
+                    <!-- Sign-in -->
+                    <div class="col-md-6 col-sm-6 sign-in">
+                        <h4 class="">Forgot Password</h4>
+                        <p class="">Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.</p>
+                        @if (session('status'))
+                            <div class="mb-4 font-medium text-sm text-danger">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
+                        <form method="POST" action="{{ route('password.email') }}"
+                              class="register-form outer-top-xs">
+                            @csrf
 
-            <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+                            <div class="form-group">
+                                <label class="info-title" for="email">Email Address <span>*</span></label>
+                                <input type="email" name="email" id="email" :value="old('email')"
+                                       class="form-control unicase-form-control text-input" required>
+                            </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+                            <button type="submit" class="btn-upper btn btn-info checkout-page-button">Email Password Reset Link</button>
+                        </form>
+                    </div>
+                    <!-- Sign-in -->
+
+                </div><!-- /.row -->
+            </div><!-- /.sigin-in-->
+            @include('user-view.layouts.components._brands')
+        </div><!-- /.container -->
+    </div><!-- /.body-content -->
+
+@endsection
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,36 +1,64 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+@extends('user-view.layouts.app')
+@section('content')
 
-        <x-jet-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('password.update') }}">
-            @csrf
+    <div class="breadcrumb">
+        <div class="container">
+            <div class="breadcrumb-inner">
+                <ul class="list-inline list-unstyled">
+                    <li><a href="{{route('home')}}">Home</a></li>
+                    <li class='active'>Reset password</li>
+                </ul>
+            </div><!-- /.breadcrumb-inner -->
+        </div><!-- /.container -->
+    </div><!-- /.breadcrumb -->
 
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+    <div class="body-content">
+        <div class="container">
+            <div class="sign-in-page">
+                <div class="row">
+                    <!-- Sign-in -->
+                    <div class="col-md-6 col-sm-6 sign-in">
+                        <h4 class="">Reset Password</h4>
 
-            <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
-            </div>
+                        @if (session('status'))
+                            <div class="mb-4 font-medium text-sm text-danger">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
+                        <form method="POST" action="{{ route('password.update') }}"
+                              class="register-form outer-top-xs">
+                            @csrf
+                            <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
+                            <div class="form-group">
+                                <label class="info-title" for="email">Email Address <span>*</span></label>
+                                <input type="email" name="email" id="email" :value="old('email', $request->email)"
+                                       class="form-control unicase-form-control text-input" required>
+                            </div>
+                            <div class="form-group">
+                                <label class="info-title" for="password">Password <span>*</span></label>
+                                <input type="password" name="password" id="password"
+                                       class="form-control unicase-form-control text-input"
+                                       required autocomplete="new-password">
+                            </div>
+                            <div class="form-group">
+                                <label class="info-title" for="password_confirmation">Confirm Password <span>*</span></label>
+                                <input type="password" name="password_confirmation" id="password_confirmation"
+                                       class="form-control unicase-form-control text-input"
+                                       required autocomplete="new-password">
+                            </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
-                    {{ __('Reset Password') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+                            <button type="submit" class="btn-upper btn btn-primary checkout-page-button"> Reset Password </button>
+                        </form>
+                    </div>
+                    <!-- Sign-in -->
+
+                </div><!-- /.row -->
+            </div><!-- /.sigin-in-->
+            @include('user-view.layouts.components._brands')
+        </div><!-- /.container -->
+    </div><!-- /.body-content -->
+
+@endsection
