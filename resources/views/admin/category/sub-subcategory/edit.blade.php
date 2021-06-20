4dd0888859db3ea8079711124d23e37fbe0f609a
Monir Hossain <monir.hossain@northsouth.edu>
2021-06-20 16:58:50 +0600

    style(admin subcategory ): update and delete complete

--- resources/views/admin/category/sub-subcategory/edit.blade.php
+++ resources/views/admin/category/sub-subcategory/edit.blade.php
@@ -102,27 +102,3 @@
     </div>
     <!-- /.content-wrapper -->
 @endsection
-@section('script')
-    <script type="text/javascript">
-        $(document).ready(function () {
-            $('select[name="category_id"]').on('change', function () {
-                let category_id = $(this).val();
-                if (category_id) {
-                    $.ajax({
-                        url: "{{url('/category/sub-subcategories')}}/" + category_id,
-                        type: "GET",
-                        dataType: "json",
-                        success: function (data) {
-                            let d = $('select[name="subcategory_id"]').empty();
-                            $.each(data, function (key, value) {
-                                $('select[name="subcategory_id"]').append('<option value="' + value.id + '">' + value.name_eng + '</option>');
-                            });
-                        },
-                    });
-                } else {
-                    alert('danger');
-                }
-            });
-        });
-    </script>
-@endsection
