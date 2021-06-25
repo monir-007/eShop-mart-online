632ff68ef7dc48a3966b3cf0d72bed10ffeeaef2
Monir Hossain <monir.hossain@northsouth.edu>
2021-06-26 05:47:53 +0600

    refactor(frontend product wishlist): wishlist with Auth validity

--- app/Http/Controllers/UserView/WishlistController.php
+++ app/Http/Controllers/UserView/WishlistController.php
@@ -13,17 +13,17 @@ class WishlistController extends Controller
     public function addToWishlist($product_id, Request $request)
     {
         if (Auth::check()) {
-            $userExists = Wishlist::where('user_id', Auth::id())->where('product_id', $product_id)->first();
-            Wishlist::insert([
-                'user_id' => Auth::id(),
-                'product_id' => $product_id,
-                'created_at' => Carbon::now()
-
-            ]);
-            return response()->json(['success' => "Product added to your Wishlist."]);
-
-        } else {
-            return response()->json(['error' => "You haven't login yet. please Login First"]);
+            $productExists = Wishlist::where('user_id', Auth::id())->where('product_id', $product_id)->first();
+            if (!$productExists) {
+                Wishlist::insert([
+                    'user_id' => Auth::id(),
+                    'product_id' => $product_id,
+                    'created_at' => Carbon::now()
+                ]);
+                return response()->json(['success' => "Product added to your Wishlist."]);
+            }
+            return response()->json(['error' => "Product has already exists in your Wishlist."]);
         }
+        return response()->json(['error' => "You haven't login yet. please Login First"]);
     }
 }
